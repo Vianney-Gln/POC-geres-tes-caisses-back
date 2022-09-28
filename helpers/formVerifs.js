a5bd2b43bf675a53f9a84fbc2b20ba9b2042197f
Vianney-Gln <vgeloen@gmail.com>
2022-09-28 17:55:01 +0200

    server side form reception protection ok

--- helpers/formVerifs.js
+++ helpers/formVerifs.js
@@ -2,10 +2,13 @@ const Joi = require("joi");
 
 // Function checking if uuid and id_article are compliant -- escape specs chars
 const validateReception = (data) => {
+  const year = new Date().getFullYear().toString();
   return Joi.object({
     uuid: Joi.string()
       .presence("required")
-      .regex(/[$\(\)<>]/, { invert: true }),
+      .length(10)
+      .regex(/^${year}/)
+      .regex(/^[0-9]+$/),
     id_article: Joi.number().presence("required"),
   }).validate(data, { abortEarly: false, allowUnknown: true }).error;
 };
